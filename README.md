ea8ebfbb8b85a152c9d0a29af7728eed15ce20d7
CircleCI <systems@spiritedmedia.com>
2018-11-30 18:52:38 +0000

    Build #6264 by montchr on 2018-11-30 06:52 PM UTC

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-Build [#6257](https://circleci.com/gh/spiritedmedia/spiritedmedia/6257) by montchr at 2018-11-29 03:27 PM UTC
+Build [#6264](https://circleci.com/gh/spiritedmedia/spiritedmedia/6264) by montchr at 2018-11-30 06:52 PM UTC
 
-[https://github.com/spiritedmedia/spiritedmedia/compare/v7.11.0](https://github.com/spiritedmedia/spiritedmedia/compare/v7.11.0)
+[https://github.com/spiritedmedia/spiritedmedia/compare/v7.11.1](https://github.com/spiritedmedia/spiritedmedia/compare/v7.11.1)
 ## Change log
-[Full Changelog](git@github.com:spiritedmedia/spiritedmedia.git/compare/v7.10.13...v7.11.0)
+[Full Changelog](git@github.com:spiritedmedia/spiritedmedia.git/compare/v7.11.0...v7.11.1)
 
- - [#2995](git@github.com:spiritedmedia/spiritedmedia.git/pull/2995): Conversion Prompts
- - [#3032](git@github.com:spiritedmedia/spiritedmedia.git/pull/3032): Use Boolean Values for Cookie Subscriber Check Data and Not Numeric Values
+ - [#3033](git@github.com:spiritedmedia/spiritedmedia.git/pull/3033): Cross Domain Tracking of Form Submissions Needs an Extra Query Parameter
--- wp-content/themes/pedestal/assets/js/theme/components/donateForm.js
+++ wp-content/themes/pedestal/assets/js/theme/components/donateForm.js
@@ -1,56 +1,65 @@
+/* global gaLinkTrackerParam */
+
 export default function handleDonateForm() {
   const $form = $('.js-donate-form');
-
-  updateFormActionURL();
-  calculateAmountForPeriod();
 
   /**
    * Handle the changing donation form action URL
    *
    * Recurring and one-time payments have different action URLs.
-   *
-   * @param  {jQuery} $form jQuery object of the donate form
    */
-  function updateFormActionURL() {
-    const endpointDomain = $form.data('nrh-endpoint-domain');
+  const endpointDomain = $form.data('nrh-endpoint-domain');
 
-    $form.on('change', '.js-donate-form-frequency', function() {
-      let endpointPath;
+  $form.on('change', '.js-donate-form-frequency', function() {
+    let endpointPath;
 
-      // Handle different action URLs for one-time vs. recurring donations
-      if ($(this).val() === '') {
-        endpointPath = '/donateform';
-      } else {
-        endpointPath = '/memberform';
-      }
-      $form.attr('action', endpointDomain + endpointPath);
-    });
-  }
+    // Handle different action URLs for one-time vs. recurring donations
+    if ($(this).val() === '') {
+      endpointPath = '/donateform';
+    } else {
+      endpointPath = '/memberform';
+    }
+    $form.attr('action', endpointDomain + endpointPath);
+  });
+
+  /**
+   * Append the Google Analytics link tracker param
+   * on form submission
+   */
+  $form.on('submit', function() {
+    if (typeof gaLinkTrackerParam == 'undefined') {
+      return;
+    }
+    const parts = gaLinkTrackerParam.split('=');
+    if (parts.length !== 2) {
+      return;
+    }
+    /* eslint-disable max-len */
+    $form.append(`<input type="hidden" name="${parts[0]}" value="${parts[1]}">`);
+  });
 
   /**
    * Calculate the amount input value based on the selected installation period
    */
-  function calculateAmountForPeriod() {
-    let prevPeriod;
+  let prevPeriod;
 
-    $form.on('change', '.js-donate-form-frequency', function() {
-      const $amountInput = $form.find('.js-donate-form-amount');
-      const currentPeriod = $(this).val();
-      const oldAmount = parseInt($amountInput.val());
-      let newAmount = oldAmount;
+  $form.on('change', '.js-donate-form-frequency', function() {
+    const $amountInput = $form.find('.js-donate-form-amount');
+    const currentPeriod = $(this).val();
+    const oldAmount = parseInt($amountInput.val());
+    let newAmount = oldAmount;
 
-      if (
-        (currentPeriod === 'yearly' && prevPeriod !== '') ||
+    if (
+      (currentPeriod === 'yearly' && prevPeriod !== '') ||
         (currentPeriod === '' && prevPeriod !== 'yearly')
-      ) {
-        newAmount = oldAmount * 12;
-      } else if (currentPeriod === 'monthly') {
-        newAmount = oldAmount / 12;
-      }
+    ) {
+      newAmount = oldAmount * 12;
+    } else if (currentPeriod === 'monthly') {
+      newAmount = oldAmount / 12;
+    }
 
-      newAmount = Math.ceil(newAmount);
-      $amountInput.val(newAmount);
-      prevPeriod = currentPeriod;
-    });
-  }
+    newAmount = Math.ceil(newAmount);
+    $amountInput.val(newAmount);
+    prevPeriod = currentPeriod;
+  });
 }
